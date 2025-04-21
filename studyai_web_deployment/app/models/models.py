c1df87997b18adba9ca03e49f6b2e95b839d54d0
Abel Paul John <145491869+Abelpaulj@users.noreply.github.com>
2025-04-21 14:13:00 +0200

    Update models.py

--- studyai_web_deployment/app/models/models.py
+++ studyai_web_deployment/app/models/models.py
@@ -1,6 +1,6 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from studyai_web_deployment.app import db
 from datetime import datetime
 
 class User(db.Model, UserMixin):
