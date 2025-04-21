689bd43267f00c42bba2b313cf5450c68f5016f7
Abel Paul John <145491869+Abelpaulj@users.noreply.github.com>
2025-04-21 14:15:48 +0200

    Update auth.py

--- studyai_web_deployment/app/routes/auth.py
+++ studyai_web_deployment/app/routes/auth.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
-from app.models.models import User
-from app import db
-from app.routes.forms import LoginForm, RegistrationForm
+from studyai_web_deployment.app.models.models import User
+from studyai_web_deployment.app import db
+from studyai_web_deployment.app.routes.forms import LoginForm, RegistrationForm
+
 
 auth = Blueprint('auth', __name__)
 
