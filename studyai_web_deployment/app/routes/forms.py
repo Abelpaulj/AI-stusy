176cf0d59e954a37b7e8e2112c2a6f7c0c29a737
Abel Paul John <145491869+Abelpaulj@users.noreply.github.com>
2025-04-21 14:16:07 +0200

    Update forms.py

--- studyai_web_deployment/app/routes/forms.py
+++ studyai_web_deployment/app/routes/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
-from app.models.models import User
+from studyai_web_deployment.app.models.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
